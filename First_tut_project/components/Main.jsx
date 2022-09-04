dd040f0c785a57336160b205462550bab3ca667e
Julian <53325429+LittieRain@users.noreply.github.com>
2022-09-04 22:02:44 +1000

    asdasd
    asdasd

--- First_tut_project/components/Main.jsx
+++ First_tut_project/components/Main.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export default function Main(){
     return(
         <main>
-            <h1 className="main_titile">Fun facts about React</h1>
-            <ul className="main_facts_list">
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
                 <li >Was first released in 2013</li>
                 <li >Was originally created by Jordan Walke</li>
                 <li >Has well over 100k stars on Github</li>
